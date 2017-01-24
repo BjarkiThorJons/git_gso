562252e4dfd28185fd3beaa3359ad944159a6745
BjarkiThorJons <bjarkijo262@nemi.tskoli.is>
2017-01-24 11:22:23 +0000

    Liður 3

--- texti.py
+++ texti.py
@@ -2,3 +2,10 @@
 text_file=open("texti.txt", "w")
 text_file.write("Halló hvað segir þú gott?")
 text_file.close()
+
+text_file=open("texti.txt", "a")
+text_file.write("\n" + "bla bla bla bla")
+text_file.write("\n" + "bla bla bla bla")
+text_file.write("\n" + "bla bla bla bla")
+
+
